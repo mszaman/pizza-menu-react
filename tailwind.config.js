675e3405ed4f321e78eba1b463a4c452d20dd95a
mszaman <munshisayeduzzaman@gmail.com>
2024-01-18 19:04:05 +0600

    configure project.

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto Mono", "monospace"],
+      },
+      colors: {
+        gold: {
+          0: "#f7f2e9",
+          1: "#edc84b",
+          2: "#e9bb24",
+          3: "#af8602",
+        },
+        grey: {
+          1: "#888",
+          2: "#252525",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
