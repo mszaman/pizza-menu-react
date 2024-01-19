d5e69eb81e11fa89a43a477b570abd0041c46a7e
mszaman <munshisayeduzzaman@gmail.com>
2024-01-19 14:07:02 +0600

    added conditional rendering for sold out pizzas.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -11,18 +11,22 @@ export default function Menu() {
           Authentic Italian cuisine. 6 creative dishes to choose from. All from
           our stone oven, all organic, all delicious.
         </p>
-        <ul className="tablet:grid-cols-2 tablet:gap-8 grid grid-cols-1 gap-16">
+        <ul className="grid grid-cols-1 gap-16 tablet:grid-cols-2 tablet:gap-8">
           {pizzas.map((p) => (
-            <li className="tablet:flex tablet:mx-0 tablet:items-start tablet:w-full mx-auto h-fit w-4/5 flex-row gap-5 text-center">
+            <li className="mx-auto h-fit w-4/5 flex-row gap-5 text-center tablet:mx-0 tablet:flex tablet:w-full tablet:items-start">
               <img
-                className="tablet:min-w-1/2 tablet:min-h-full tablet:object-contain tablet:mb-0 tablet:mx-0 tablet:aspect-square mx-auto mb-5 w-32"
+                className={`tablet:min-w-1/2 mx-auto mb-5 w-32 tablet:mx-0 tablet:mb-0 tablet:aspect-square tablet:min-h-full tablet:object-contain ${p.soldOut ? "grayscale" : ""}`}
                 src={p.photoName}
                 alt={p.name}
               />
-              <div className="tablet:items-start tablet:text-start flex flex-col gap-3">
+              <div
+                className={`flex flex-col gap-3 tablet:items-start tablet:text-start ${p.soldOut ? "text-grey-1" : ""}`}
+              >
                 <h4 className="text-xl font-medium">{p.name}</h4>
                 <p className="italic">{p.ingredients}</p>
-                <p className="text-lg font-medium">$ {p.price}</p>
+                <p className="text-lg font-medium uppercase">
+                  {p.soldOut ? "sold out" : `$${p.price}`}
+                </p>
               </div>
             </li>
           ))}
